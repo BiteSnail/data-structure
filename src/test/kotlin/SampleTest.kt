e2ab905d01f1101064c4804b8995f7ff96bebe6e
bitesnail <rkwoal216@gmail.com>
2024-09-10 11:02:02 +0900

    Add wrong answer test for sum

--- src/test/kotlin/SampleTest.kt
+++ src/test/kotlin/SampleTest.kt
@@ -15,4 +15,15 @@ class SampleTest {
 
         assertEquals(expectedSum, actualSum)
     }
+
+    @Test
+    fun wrongAnswer() {
+        val expectedSum = 12
+        val a= 3
+        val b = 8
+
+        val actualSum = testSample.sum(a, b)
+
+        assertEquals(expectedSum, actualSum)
+    }
 }

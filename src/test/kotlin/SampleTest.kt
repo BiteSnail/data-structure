067ed5d75fb293ffb77a15aeb250d1d20671056d
bitesnail <rkwoal216@gmail.com>
2024-09-10 11:00:16 +0900

    Add sample sum function and test

---
+++ src/test/kotlin/SampleTest.kt
@@ -0,0 +1,18 @@
+import org.example.Sample
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    private val testSample: Sample = Sample()
+
+    @Test
+    fun testSum() {
+        val expectedSum = 12
+        val a = 3
+        val b = 9
+
+        val actualSum = testSample.sum(a, b)
+
+        assertEquals(expectedSum, actualSum)
+    }
+}

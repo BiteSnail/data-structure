bfe60e263707a15cac8aa713a502220e28bb09c0
bitesnail <rkwoal216@gmail.com>
2024-09-23 18:38:53 +0900

    add payment factory with companion object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,12 @@
 package org.example
 
-import org.example.entities.BitcoinPayment
-import org.example.entities.CreditCardPayment
-import org.example.entities.PayPalPayment
+import org.example.interfaces.Payment
 
 fun main() {
     println("결제 수단을 입력해 주세요.")
     val type = readln()
 
-    // TODO(적절한 Payment 구체화 객체를 생성하는 로직을 별도의 팩토리 클래스로 만들어 보자.)
-    val payment = when (type) {
-        "bitcoin" -> BitcoinPayment()
-        "paypal" -> PayPalPayment()
-        "credit" -> CreditCardPayment()
-        else -> throw IllegalArgumentException("지원하지 않는 결제 방식입니다.")
-    }
+    val payment = Payment.create(type)
 
     payment?.pay(11111.0) ?: "잘못된 접근입니다."
 }
--- src/main/kotlin/interfaces/Payment.kt
+++ src/main/kotlin/interfaces/Payment.kt
@@ -1,5 +1,21 @@
 package org.example.interfaces
 
+import org.example.entities.BitcoinPayment
+import org.example.entities.CreditCardPayment
+import org.example.entities.PayPalPayment
+
 interface Payment {
     fun pay(amount: Double)
+
+    // TODO("Payment를 상속받는 새로운 결제 방식 MobilePayment를 생성해보자")
+    companion object {
+        fun create(type: String): Payment {
+            return when (type) {
+                "bitcoin" -> BitcoinPayment()
+                "credit" -> CreditCardPayment()
+                "paypal" -> PayPalPayment()
+                else -> throw IllegalArgumentException("지원하지 않는 결제 방식입니다.")
+            }
+        }
+    }
 }

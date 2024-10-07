0b8b1022a454fb6e4e5f3ed2d86d7d15f36d5e79
bitesnail <rkwoal216@gmail.com>
2024-10-07 14:31:35 +0900

    add simple codeˆ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    val map: Map<String, Int> = mapOf("Math" to 99, "Korean" to 88, "Science" to 90);
+
+    println("전체 평균은 ${map.values.sum()}점 입니다.")
 }
